adef0eec1d1d46dfd4a8838a14a84379bd6650f7
LabUser <seanarmandroberts@outlook.com>
2023-05-08 08:13:02 -0600

    Finished Javadocs up to users

--- src/main/java/com/example/WGUSoftware2/model/Contacts.java
+++ src/main/java/com/example/WGUSoftware2/model/Contacts.java
@@ -1,36 +1,68 @@
 package com.example.WGUSoftware2.model;
 
+/**
+ * Contacts class that contains functions for getting, setting, and constructing contacts
+ */
 public class Contacts {
     private Integer contactID;
     private String contactName;
     private String email;
 
+    /**
+     * Constructor for contacts
+     * @param contactID contact ID
+     * @param contactName contact name
+     * @param email contact email
+     */
     public Contacts(Integer contactID, String contactName, String email) {
         this.contactID = contactID;
         this.contactName = contactName;
         this.email = email;
     }
 
+    /**
+     * Getter - contact ID.
+     * @return ID of the contact.
+     */
     public Integer getContactID() {
         return contactID;
     }
-
+    /**
+     * Setter - contact ID.
+     * @param contactID the contact ID to set.
+     */
     public void setContactID(Integer contactID) {
         this.contactID = contactID;
     }
 
+    /**
+     * Getter - contact name.
+     * @return name of the contact.
+     */
     public String getContactName() {
         return contactName;
     }
 
+    /**
+     * Setter - contact name.
+     * @param contactName the contact name to set.
+     */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
+    /**
+     * Getter - contact email.
+     * @return email of the contact.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter - contact email.
+     * @param email the contact email to set.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
--- src/main/java/com/example/WGUSoftware2/model/CurrentUser.java
+++ src/main/java/com/example/WGUSoftware2/model/CurrentUser.java
@@ -8,20 +8,36 @@ import java.sql.SQLException;
 import java.time.ZoneId;
 import java.util.Locale;
 
+/**
+ * CurrentUser class that contains functions for getting, setting, and constructing the current user that logged in
+ */
 public class CurrentUser {
 
     private String username;
     private Integer userID;
 
+    /**
+     * Constructor for current user
+     * @param username name of user
+     * @param userID ID of user
+     */
     public CurrentUser(String username, Integer userID){
         this.username = username;
         this.userID = userID;
     }
 
+    /**
+     * Getter - user name.
+     * @return name of the user.
+     */
     public String getUsername(){
         return this.username;
     }
 
+    /**
+     * Getter - user ID.
+     * @return ID of the user.
+     */
     public Integer getUserID(){
         return this.userID;
     }
